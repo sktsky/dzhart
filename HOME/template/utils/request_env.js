f2bd03d185f3bd104fc377ae1872cecee9ee2e0d
yeyou33 <1909794039@qq.com>
2019-03-19 17:35:17 +0800

    esc

--- HOME/template/utils/request_env.js
+++ HOME/template/utils/request_env.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 创建axios实例
 const service = axios.create({
-    baseURL: 'https://localhost:80', // api的base_url
+    baseURL: '', // api的base_url
     timeout: 30000 // 请求超时时间
   })
 export default service
