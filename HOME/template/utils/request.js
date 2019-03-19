508dee640750968ddc3f3bbb793383f907cf7773
luozhou <332886253@qq.com>
2019-03-19 18:04:25 +0800

    777

--- HOME/template/utils/request.js
+++ HOME/template/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // 创建axios实例
 const service = axios.create({
-  baseURL: '', // api的base_url
+  baseURL: 'localhost://8082', // api的base_url
   timeout: 5000 // 请求超时时间
 
 })
